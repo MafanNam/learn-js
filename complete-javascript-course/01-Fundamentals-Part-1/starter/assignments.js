4b48649993f2b1cf5c7c3cdbe522024231279d20
Maxim Manzik <kepkpkep@gmail.com>
2024-01-07 23:35:34 +0200

    done tasks assignments

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,21 @@
+// let country = 'Ukraine';
+// let continent = 'Europa';
+// let population = 'People';
+//
+// console.log('Hello I from', country,
+//     'this is continent named as', continent,
+//     'and my population is', population)
+
+
+// let language = 'Ukraine';
+// console.log(language)
+// language = 'UpdateLanguage';
+// console.log(language)
+//
+// const apiKey = 'jdjddj384848';
+// console.log(apiKey)
+//
+// // apiKey = 'ff'
+//
+// language = 'Up2';
+// console.log(language)
---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,56 @@
+// let js = 'hahaah';
+// if (js === 'hahaah')
+//     // alert("JavaScript is FUN");
+//
+// console.log(40 + 8 + 23 - 10);
+// console.log(23);
+//
+// let firstName = 'Max';
+// let first = 'jonas';
+// let firstNamePerson;
+// let first_name_person;
+// console.log(firstName);
+// console.log(firstNamePerson);
+// console.log(first_name_person);
+//
+// // let 3years = '';
+//
+// let javaBool = true;
+// console.log(javaBool)
+//
+// console.log(typeof javaBool);
+// console.log(typeof 'ddd');
+// console.log(typeof 33);
+// console.log(typeof 33.44);
+// console.log(typeof 33/3);
+//
+// javaBool = 'not bool';
+// console.log(typeof javaBool);
+//
+// let isIsland = true;
+// let language;
+// console.log(isIsland, language)
+
+
+// let age = 20;
+// age += 1;
+// console.log(age)
+
+// const birthYear = 1999;
+// birthYear = 1900;
+
+const now = 2024
+const ageJonas = now - 1991;
+// const ageJonas = 2000 - 1991;
+console.log(ageJonas);
+
+console.log(ageJonas * 2, ageJonas / 2, ageJonas % 2, ageJonas ** 2)
+
+
+let x = 0;
+++x;
+x++;
+x--;
+--x;
+console.log(x)
+
