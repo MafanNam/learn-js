3c338e1986853ee7f0195d2eac026ee18a4dbba1
Maxim Manzik <kepkpkep@gmail.com>
2024-01-13 00:09:56 +0200

    complete Modal Window project.

--- complete-javascript-course/06-Modal/starter/script.js
+++ complete-javascript-course/06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+    // console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+// console.log(btnOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModel);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    // console.log('A key was pressed');
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
+
+
