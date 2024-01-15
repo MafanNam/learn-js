8993b65c7f4b414c5ec32accd31fdb7a419ea4f3
Maxim Manzik <kepkpkep@gmail.com>
2024-01-15 20:16:33 +0200

    Destructuring Arrays, Objects, ..., && and ||, ??, code challenge.

---
+++ complete-javascript-course/09-Data-Structures-Operators/starter/assignments.js
@@ -0,0 +1,39 @@
+// const books = ['OneBook', 'TwoBook', 'ThirdBook'];
+//
+// const [firstBook, secondBook] = books;
+//
+// console.log(firstBook, secondBook);
+//
+// const [, , thirdBook] = books;
+// console.log(thirdBook);
+//
+//
+// const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+// const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
+//
+//
+// const ratingStars = [63405, 1808];
+// const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+// const books = [{title:'OneBook', author: 'Me', ISBN: 555, keywords: ['1', '2']},
+//     'TwoBook', {title: 'ThirdBook', language: 'US'}];
+//
+// const {title, author, ISBN} = books[0];
+// console.log(title, author, ISBN);
+//
+// const {keywords: tags} = books[0];
+// console.log(tags);
+//
+// const {language, programmingLanguage = 'unknown'} = books[2];
+// console.log(language, programmingLanguage);
+//
+// let bookTitle = 'unknown';
+// let bookAuthor = 'unknown';
+//
+// ({title: bookTitle, author: bookAuthor} = books[0]);
+// console.log();
+
+
+
