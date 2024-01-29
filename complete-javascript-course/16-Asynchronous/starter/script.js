09b507dc6385788adc3f5eeb5ecf01978aaedad0
Maxim Manzik <kepkpkep@gmail.com>
2024-01-29 15:34:42 +0200

    event loop, build promise, geo api, challenge.

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -146,3 +146,100 @@ const countriesContainer = document.querySelector('.countries');
 //
 // // getCountryData('germanfff');
 
+
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+//
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// Promise.resolve('Resolved promise 2').then(res => {
+//     for (let i = 0; i < 1000000000; i++) {
+//         i++;
+//     }
+//     console.log(res);
+// });
+//
+// console.log('Test end');
+
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//
+//     console.log('Lottery draw is happening')
+//     setTimeout(function () {
+//         if (Math.random() >= 0.5) {
+//             resolve('You WIN 👌');
+//         } else {
+//             reject(new Error('You lost your money 🪫'));
+//         }
+//     }, 2000);
+// });
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// const wait = function (seconds) {
+//     return new Promise(function (resolve) {
+//         setTimeout(resolve, seconds * 1000);
+//     });
+// };
+//
+// wait(2).then(() => {
+//     console.log('I waited for 2 seconds');
+//     return wait(1);
+// }).then(() => console.log('I waited for 1'));
+//
+//
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('abc')).catch(x => console.error(x));
+
+
+
+// console.log('Getting position');
+
+
+// const getPosition = function () {
+//     return new Promise(function (resolve, reject) {
+//         // navigator.geolocation.getCurrentPosition(
+//         //     position => resolve(position),
+//         //     positionError => reject(positionError),
+//         // );
+//         navigator.geolocation.getCurrentPosition(resolve, reject);
+//     });
+// };
+// console.log('hah1');
+// getPosition().then(res => console.log(res)).catch(err => console.error(new Error('EROR AAHHAH')));
+// console.log('hah2');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- complete-javascript-course/16-Asynchronous/starter/tasks.js
+++ complete-javascript-course/16-Asynchronous/starter/tasks.js
@@ -1,51 +1,94 @@
+'use strict';
 
+//
+// const renderCountry = function (data, className = '') {
+//     const html = `
+//     <article class="country ${className}">
+//           <img class="country__img" src="${data.flags.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population / 1_000_000).toFixed(1)} people</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies}</p>
+//           </div>
+//         </article>
+//     `;
+//
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = '1';
+// }
+//
+// const whereAmI = function (lat, lng) {
+//     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//         .then(res => {
+//             if (!res.ok) throw new Error(`Problems ${res.status}`)
+//             return res.json()
+//         })
+//         .then(data => {
+//             console.log(data);
+//             console.log(`You are in ${data.city}, ${data.country}`);
+//
+//             return fetch(`https://restcountries.com/v3.1/name/${data.country.toLowerCase()}`)
+//         })
+//         .then(res => {
+//             if (!res.ok)
+//                 throw new Error(`Country not found`);
+//             return res.json()
+//         })
+//         .then(data => renderCountry(data[0]))
+//         .catch(err => console.log(err));
+// }
+//
+// btn.addEventListener('click', function () {
+//     whereAmI(52.508, 13.381);
+// });
 
-const renderCountry = function (data, className = '') {
-    const html = `
-    <article class="country ${className}">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1_000_000).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages}</p>
-            <p class="country__row"><span>💰</span>${data.currencies}</p>
-          </div>
-        </article>
-    `;
+const imgContainer = document.querySelector('.images');
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = '1';
-}
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
 
-const whereAmI = function (lat, lng) {
-    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-        .then(res => {
-            if (!res.ok) throw new Error(`Problems ${res.status}`)
-            return res.json()
+const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath
+
+        img.addEventListener('load', function () {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function () {
+            reject(new Error('Image not found'));
         })
-        .then(data => {
-            console.log(data);
-            console.log(`You are in ${data.city}, ${data.country}`);
+    });
+};
 
-            return fetch(`https://restcountries.com/v3.1/name/${data.country.toLowerCase()}`)
-        })
-        .then(res => {
-            if (!res.ok)
-                throw new Error(`Country not found`);
-            return res.json()
-        })
-        .then(data => renderCountry(data[0]))
-        .catch(err => console.log(err));
-}
+let currentImg;
 
-btn.addEventListener('click', function () {
-    whereAmI(52.508, 13.381);
-});
-
-
-
-
+createImage('img/img-1.jpg')
+    .then(res => {
+        currentImg = res;
+        console.log(res);
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpg')
+    })
+    .then(res => {
+        currentImg = res;
+        console.log(res);
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err));
 
 
 
