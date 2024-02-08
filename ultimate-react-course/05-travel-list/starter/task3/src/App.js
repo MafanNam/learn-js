5c94a51f3bcc2b7923bb2ccdbdf75cae48ed3017
Maxim Manzik <kepkpkep@gmail.com>
2024-02-08 18:43:48 +0200

    children prop, refactoring. task4 and challenge.

--- ultimate-react-course/05-travel-list/starter/task3/src/App.js
+++ ultimate-react-course/05-travel-list/starter/task3/src/App.js
@@ -28,20 +28,22 @@ export default function App() {
 }
 
 function Accordion() {
+  const [curOpen, setIsOpen] = useState(null);
+
   return (
     <div className="accordion">
       {faqs.map((faq, i) =>
-        <AccordionItem
-          num={i} title={faq.title} text={faq.text} key={i}/>)}
+        <AccordionItem curOpen={curOpen} onOpen={setIsOpen}
+                       num={i} title={faq.title} key={i}>{faq.text}</AccordionItem>)}
     </div>
   );
 }
 
-function AccordionItem({num, title, text}) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({num, title, curOpen, onOpen, children}) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen(isOpen => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -49,7 +51,7 @@ function AccordionItem({num, title, text}) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
