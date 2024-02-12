346556795241bfbef594a5bb0ff4ed10fcfc3805
Maxim Manzik <kepkpkep@gmail.com>
2024-02-12 15:46:43 +0200

    useRef, custom useHook, refactoring proj usePopcorn, challenge.

--- ultimate-react-course/07-usepopcorn/starter/usepopcorn/src/AppV2.js
+++ ultimate-react-course/07-usepopcorn/starter/usepopcorn/src/AppV2.js
@@ -1,24 +1,23 @@
-import {Fragment, useEffect, useRef, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import './index.css';
 import StarRating from "./StarRating";
-import {useMovies} from "./useMovies";
-import {useLocalStorageState} from "./useLocalStorageState";
-import {useKey} from "./useKey";
-
-const KEY = 'f84fc31d';
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+const KEY = 'f84fc31d';
 
 function Loader() {
   return <p className="loader">Loading...</p>;
 }
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
   const [query, setQuery] = useState("interstellar");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
-  const {movies, isLoading, error} = useMovies(query);
-  const [watched, setWatched] = useLocalStorageState([], 'watched');
 
   function handleSelectMovie(id) {
     setSelectedId(selectedId => selectedId === id ? null : id);
@@ -35,6 +34,49 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched(watched => watched.filter((movie => movie.imdbID !== id)));
   }
+
+
+  useEffect(function () {
+    const controller = new AbortController();
+
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        setError('');
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          {signal: controller.signal}
+        );
+
+        if (!res.ok) throw new Error('Something went wrong with fetching movies');
+
+        const data = await res.json();
+
+        if (data.Response === 'False') throw new Error('Movie not found');
+
+        setMovies(data.Search);
+        setError('');
+      } catch (err) {
+
+        if (err.name !== "AbortError") setError(err.message);
+
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (query.length < 3) {
+      setMovies([]);
+      setError('');
+      return;
+    }
+
+    fetchMovies();
+
+    return function () {
+      controller.abort();
+    };
+  }, [query]);
 
   return (
     <Fragment>
@@ -93,14 +135,6 @@ function Logo() {
 }
 
 function Search({query, setQuery}) {
-  const inputEl = useRef(null);
-
-  useKey('Enter', function () {
-    if (document.activeElement === inputEl.current) return;
-
-    inputEl.current.focus();
-    setQuery('');
-  })
 
   return (
     <input
@@ -109,7 +143,6 @@ function Search({query, setQuery}) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
-      ref={inputEl}
     />
   );
 }
@@ -180,15 +213,8 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watched}) {
   const [userRating, setUserRating] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-  const countRef = useRef(0);
-
-  useEffect(() => {
-    if (userRating) countRef.current++;
-  }, [userRating]);
-
   const isWatched = watched.map(movie => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(movie => movie.imdbID === selectedId)?.userRating;
-
 
   const {
     Title: title,
@@ -212,13 +238,22 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watched}) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(' ').at(0)),
       userRating,
-      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
 
-  useKey('Escape', onCloseMovie);
+  useEffect(function () {
+    function callback(e) {
+      if (e.code === 'Escape') onCloseMovie();
+    }
+
+    document.addEventListener('keydown', callback);
+
+    return function () {
+      document.removeEventListener('keydown', callback);
+    };
+  }, [onCloseMovie]);
 
   useEffect(function () {
     async function getMovieDetails() {
