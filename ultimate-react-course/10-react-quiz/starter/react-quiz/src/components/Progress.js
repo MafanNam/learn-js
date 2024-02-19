8cf6ee9a3471e8bae2c3ba4b103f0cd69632b013
Maxim Manzik <kepkpkep@gmail.com>
2024-02-19 13:42:51 +0200

    add fake auth protect route, complete challenge(refactoring react-quiz proj for context api)

--- ultimate-react-course/10-react-quiz/starter/react-quiz/src/components/Progress.js
+++ ultimate-react-course/10-react-quiz/starter/react-quiz/src/components/Progress.js
@@ -1,7 +1,11 @@
-export default function Progress({index, numQuestions, points, maxPossiblePoints, answer}) {
+import {useQuiz} from "../context/QuizContext";
+
+export default function Progress() {
+  const {index, numQuestions, points, maxPossiblePoints, answer} = useQuiz();
+
   return (
     <header className="progress">
-      <progress max={numQuestions} value={index + +(answer !== null)} />
+      <progress max={numQuestions} value={index + +(answer !== null)}/>
 
       <p>Question <strong>{index + 1}</strong> / {numQuestions}</p>
       <p><strong>{points}</strong> / {maxPossiblePoints}</p>
