d1f18e9c4d271aab6d2edcf2d61352bdb4909627
Maxim Manzik <kepkpkep@gmail.com>
2024-02-29 19:33:26 +0200

    provide all auth functionality(login/logout/signup), user update and password, add DarkMode.

--- ultimate-react-course/17-the-wild-oasis/starter/the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ ultimate-react-course/17-the-wild-oasis/starter/the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import {useState} from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
+import FormRowVertical from "../../ui/FormRowVertical.jsx";
+import {useLogin} from "./useLogin.js";
+import SpinnerMini from "../../ui/SpinnerMini.jsx";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("admin@gmail.com");
+  const [password, setPassword] = useState("maks2004");
 
-  function handleSubmit() {}
+  const {login, isLoading} = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({email, password}, {
+      onSettled: () => {
+        setEmail('');
+        setPassword('');
+      }
+    });
+
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -19,6 +34,7 @@ function LoginForm() {
           // This makes this form better for password managers
           autoComplete="username"
           value={email}
+          disabled={isLoading}
           onChange={(e) => setEmail(e.target.value)}
         />
       </FormRowVertical>
@@ -28,11 +44,14 @@ function LoginForm() {
           id="password"
           autoComplete="current-password"
           value={password}
+          disabled={isLoading}
           onChange={(e) => setPassword(e.target.value)}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? 'Log in' : <SpinnerMini/>}
+        </Button>
       </FormRowVertical>
     </Form>
   );
